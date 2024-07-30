e4910f59173fff1ea4cf39a89cf7f9e5bb882b88
dev4 <dev4@bemagro.com>
2024-07-30 18:28:01 -0300

    ITSM-1931: Remove Z coords

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,6 @@
 from scripts.UTMZoneFinder import UTMZoneFinder
 from osgeo import ogr 
+import shapely
 
 def get_utm(shape_df, src_crs:str='4326'):
     
@@ -25,3 +26,25 @@ def get_correct_crs(filepath)->str:
     crs = src_file.GetLayer().GetSpatialRef().GetAttrValue("AUTHORITY",1)
 
     return crs
+
+def convert_3D_2D(geometry:shapely.geometry)->list:
+    """
+
+    Takes a 3D Multi/Polygons geometry (has_z) and returns a list of 2D Multi/Polygons
+
+    Args:
+        geometry (Union[Polygon, MultiPolygon]): shapely geometry to be converted to 2D
+
+    Returns:
+        list: list with converted 2D geometries
+    """
+    geometries_2d = []
+    printed = False 
+    for p in geometry:
+        if p. has_z:
+            if not printed: print("Polygon has z-component. Converting to planar....")
+            printed = True
+            geometries_2d.append(shapely.ops.transform(lambda x, y, z=None: (x, y), p))
+        else:
+            geometries_2d.append(p)
+    return geometries_2d
